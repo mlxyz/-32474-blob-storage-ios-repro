919d2858aacf81523dd669f572b78884a5e98cd2
Marian Lambert <marian.lambert@xpace.de>
2022-12-07 11:18:10 +0100

    Initial commit

---
+++ BlobStorageRepro/BlobStorageTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Azure.Storage.Blobs;
+using NUnit.Framework;
+
+namespace BlobStorageRepro
+{
+    [TestFixture]
+    public class BlobStorageTest
+    {
+        [Test]
+        public async void ShouldUploadWithoutError()
+        {
+            var localFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                "test.txt");
+            await File.WriteAllTextAsync(localFilePath, "this is a test text document");
+            var fileName = Path.GetFileName(localFilePath);
+            var container =
+                new BlobContainerClient("/* input connection string here */", "/* input container name here*/");
+            try
+            {
+                await container.GetBlobClient(fileName).UploadAsync(localFilePath, true);
+            }
+            catch (Exception exception)
+            {
+                Assert.Fail("Upload failed with exception: " + exception);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+﻿# MRE for [32474](https://github.com/Azure/azure-sdk-for-net/issues/32474)
+
+To reproduce the error follow these steps:
+1. Input the connection string and container name in `BlobStorageRepro/BlobStorageTest`
+2. Run the tests on an iOS device or simulator
+3. You will receive the following error:
+
+![Error](error.png)
